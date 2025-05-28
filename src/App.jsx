f9ae1b4f1abb60a9140d33ba372c1518ee9373e8
Henry Djupsjöbacka <henry.djupsjobacka@gmail.com>
2025-05-28 10:14:42 +0300

    debuggausta

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
     noteService
       .getAll()
       .then(initialNotes => {
+        console.log("Fetched notes:", initialNotes);
         setNotes(initialNotes)
       }) 
   }, [])
